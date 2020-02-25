ab8550767f1846b71fa45dc9364ea7edeaa9dd89
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-02-25 10:36:40 -0700

    editing template, adding arguments

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -7,7 +7,7 @@ class User(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
     newest_tweet_id = DB.Column(DB.BigInteger)
-    def __repr__(self):
+    def __repr__(self): # Makes the output prettier when calling in flask shell
         return '<User {}>'.format(self.name)
 
 
@@ -17,5 +17,5 @@ class Tweet(DB.Model):
     text = DB.Column(DB.Unicode(300))
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
-    def __repr__(self):
+    def __repr__(self): # Makes the output prettier when calling in flask shell
         return '<Tweet {}>'.format(self.text)
