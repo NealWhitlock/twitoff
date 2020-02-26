ad0d6bfa0f2522def8b7848ced3d57645ffd6791
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-02-26 12:24:18 -0700

    getting ready for predictions

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,11 +1,11 @@
-"""Code for our app"""
+"""Code for our app. The main application."""
 
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 # Make our app factory
-
 def create_app():
     app = Flask(__name__)
 
@@ -23,6 +23,22 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
     
+    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        # import pdb; pdb.set_trace()
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets = []
+        return render_template('user.html', title=name, 
+                               tweets=tweets, message=message)
+
     @app.route('/reset')
     def reset():
         DB.drop_all()
